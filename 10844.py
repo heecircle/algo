37668a16a5c7834d2c4e326ec25f956478140373
yhw991228 <74080194+yhw991228@users.noreply.github.com>
2021-08-17 14:38:04 +0900

    Create 10844.py

---
+++ 10844.py
@@ -0,0 +1,20 @@
+from sys import stdin
+
+
+n = int(stdin.readline())
+
+stairs = [[0] * 10 for _ in range(n + 1)]
+
+stairs[1] = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+for i in range(2, n + 1):
+    stairs[i][0] = stairs[i - 1][1]
+    stairs[i][9] = stairs[i - 1][8]
+    
+    for j in range(1, 9):
+        stairs[i][j] = stairs[i - 1][j - 1] + stairs[i - 1][j + 1]
+
+
+print(sum(stairs[n]) % 1000000000)
+
+#0으로 끝나거나 9로 끝나는 계단 따로 , 1~8로 끝나는 애들은 공통적으로 작성
